b107f9d553b5f49406a877e159f4ed59663ff2c0
lethienngan <lethienngan95@gmail.com>
2021-06-17 16:44:33 +0700

    GET/POST Method

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 const path = require('path')
 const express = require('express');
+
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-
 //Access Public file
-app.use(express.static(path.join(__dirname,'public')))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 // middleware monitor - HTTP logger
 app.use(morgan('combined'));
 //Template Engine - Rendering engine setup
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-
-app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.engine('.hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// Body-parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 
 
 // route
-app.get('/', function (req, res) {
-    return res.render('home');
-});
-app.get('/news', (req,res) => {
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/news', (req, res) => {
     res.render('news');
 })
-
-
-
-
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+app.post('/search', (req, res) => {
+    console.log('Typeof body: ', typeof req.body)
+    console.log('Get body: ', req.body)
+    res.sendStatus(200);
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
